cf832cea0fb2609f1e3d47dd5275b5346c3a5281
hhansen06 <hhansen06@googlemail.com>
2021-11-12 13:07:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-
+docker run \
+    -e UPDATES=enabled \
+    -e ALLOW_CORS=enabled \
+    -p 5432:5432 \
+    -p 8080:80 \
+    -e THREADS=24 \
+    -e "OSM2PGSQL_EXTRA_ARGS=-C 4096" \
+    -v openstreetmap-data:/var/lib/postgresql/12/main \
+    -d overv/openstreetmap-tile-server \
+    run
